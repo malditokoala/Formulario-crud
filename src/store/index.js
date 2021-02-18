0d81d34702132eeb1ac56a9edd835e8443ff24ed
Gomez, Edgar A <edgar.gomez@te.com>
2021-02-17 18:16:37 -0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    tareas: [],
+    tarea: {
+      id: "",
+      nombre: "",
+      categorias: [],
+      estado: "",
+      numero: 0,
+    },
+  },
+  mutations: {
+    set(state, payload){
+      state.tareas.push(payload);
+      console.log(state.tareas);
+    },
+    eliminar(state, payload){
+      state.tareas = state.tareas.filter(item => item.id !== payload);
+    }
+  },
+  actions: {
+    setTareas({commit}, tarea){
+      commit('set', tarea)
+    },
+    deleteTareas({commit}, id){
+      commit('eliminar', id);
+    }
+  },
+  modules: {
+  }
+})
---
+++ src/views/Editar.vue
@@ -0,0 +1,8 @@
+<template>Editar{{ $route.params.id }}</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
